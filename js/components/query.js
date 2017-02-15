c6baee3a3a1af0020a978371cfb7735c01014c00
Russell Henry <rjhenry1014@gmail.com>
2017-02-15 01:21:52 +0000

    Fixed test query call

--- js/components/query.js
+++ js/components/query.js
@@ -10,19 +10,22 @@ var Query = React.createClass({
 
     apiQuery: function(event) {
         event.preventDefault();
+        var searchQuery = this.refs.queryString.value;
+        console.log(searchQuery);
     },
 
     render: function() {
 
         return (
-          <div className="movie-query">
+          <div className="movie-query" onSubmit={this.apiQuery}>
             <p>Please enter a movie of your choice to search for.</p>
-            <form id="movieQuery" ref="queryString">
+            <form id="movieQuery">
               <input
                 type="text"
                 name="movieQueryInput"
+                ref="queryString"
               />
-              <input type="submit" name="Submit" onSubmit={this.apiQuery} value="Find Movie!"/>
+              <input type="submit" name="Submit" value="Find Movie!"/>
             </form>
           </div>
         );
