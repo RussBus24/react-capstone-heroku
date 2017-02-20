bc699f0e6d82ddb6e39a8d1527fa51125b457d5d
Russell Henry <rjhenry1014@gmail.com>
2017-02-20 17:36:21 +0000

    Added quotes component

---
+++ js/components/quotes.js
@@ -0,0 +1,14 @@
+module.exports = {
+  quotesLow: ['It appears not much phases you.',
+              'You watch too many movies.',
+              'You are a heartless just like the Kanye West song.'],
+  quotesMed: ['Seems like you have a good head on your shoulders.',
+              'You need to have a shot of whiskey.',
+              'I am a test med quote'],
+  quotesHigh: ['You must have had a sheltered childhood.',
+               'You should consider a career in politics.',
+               'You are a raging sociopath.'],
+  quotesOmg: ['ARE YOU MAD?!',
+              'Put down the alcohol please.',
+              '...I have no words.']
+}
--- js/components/result.js
+++ js/components/result.js
@@ -1,20 +1,44 @@
 var React = require('react');
 var connect = require('react-redux').connect;
+var Quotes = require('./quotes');
 
 var actions = require('../actions/index');
 
 var Result = React.createClass({
 
+  resultQuote: function() {
+    var randomPick = Math.floor(Math.random() * 3);
+  },
+
   render: function() {
 
     return (
       <div className="movie-result">
-        <h2>Based on your review versus the movie's actual rating...</h2>
-        <p>You are likely a raging sociopath.</p>
+        <h2>Based on your rating versus the movie's actual rating...</h2>
+        <h1><p>{Quotes.quotesMed[Math.floor(Math.random() * 3)]}</p></h1>
       </div>
     );
   }
 });
 
-module.exports = Result;
+var mapStateToProps = function(state, props) {
+  return {
+    violenceRating: state.violenceRating,
+    languageRating: state.languageRating,
+    sensualityRating: state.sensualityRating,
+    movieTitle: state.movieTitle,
+    movieYear: state.movieYear,
+    movieRating: state.movieRating,
+    lowValue: state.randomValueLow,
+    medValue: state.randomValueMed,
+    highValue: state.randomValueHigh,
+    omgValue: state.randomValueOmg
+  };
+};
+
+var Container = connect(mapStateToProps, null)(Result);
+
+module.exports = Container;
+
+//module.exports = Result;
 
