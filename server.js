debdc06e9d3144ba5195ef059547f43e1c1d8036
Russell Henry <russbus24@Russells-MacBook-Pro.local>
2017-02-27 19:03:15 -0500

    Updates

--- server.js
+++ server.js
@@ -4,35 +4,10 @@ var mongoose = require('mongoose');
 var jsonParser = bodyParser.json();
 var cors = require('cors');
 
-var config = require('./config');
-
 var app = express();
 app.use(cors());
 app.use(express.static('build'));
 app.use(bodyParser.json());
-
-var runServer = function(callback) {
-    mongoose.connect(config.DATABASE_URL, function(err) {
-        if (err && callback) {
-            return callback(err);
-        }
-
-        app.listen(config.PORT, function() {
-            console.log('Listening on localhost:' + config.PORT);
-            if (callback) {
-                callback();
-            }
-        });
-    });
-};
-
-if (require.main === module) {
-    runServer(function(err) {
-        if (err) {
-            console.error(err);
-        }
-    });
-}
 
 var Quotes = require('./models/quotes');
 
@@ -48,6 +23,5 @@ app.get('/fewest-guesses', function(request, response) {
 });
 
 exports.app = app;
-exports.runServer = runServer;
 
 //app.listen(process.env.PORT || 8080, process.env.IP);
